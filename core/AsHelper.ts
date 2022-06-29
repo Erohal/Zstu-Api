1bebcb3a2b425bb4b9001abe061d406a00c84c0b
Erohal <erohalthebest@gmail.com>
2022-06-29 16:54:33 +0800

    Fix the api of getting grade in as

--- core/AsHelper.ts
+++ core/AsHelper.ts
@@ -4,11 +4,9 @@ import { CookieJar } from "tough-cookie";
 import { createSession } from "../utils/session";
 
 export default class AsHelper {
-    private cookieJar: CookieJar
     private sesstion: AxiosInstance
     
     constructor(cookieJar: CookieJar) {
-        this.cookieJar = cookieJar
         this.sesstion = createSession(cookieJar)
     }
 
@@ -24,6 +22,39 @@ export default class AsHelper {
         }).then((value) => {
             return value.data
         })
-        return res
+
+        let processedRes: any = {}
+        let items = []
+        let totalCredits: number = 0
+        let totalFps: number = 0
+        /**
+         * res.items[item].cj   ---> Percentage grades
+         * res.items[item].kcmc ---> Name of the couse
+         * res.items[item].jd   ---> Five-point scale
+         * res.items[item].xf   ---> Credits
+         * res.items[item].xfjd ---> Credits * Five-point scale
+         * res.items[item].tjsj ---> Update time
+         * res.items[item].tjrxm ---> teacher
+         */
+        for (let item in res.items) {
+            let curse: any = {}
+            curse.grade = res.items[item].cj
+            curse.name = res.items[item].kcmc
+            curse.jd = res.items[item].jd
+            curse.xf = res.items[item].xf
+            curse.xfjd = res.items[item].xfjd
+            curse.time = res.items[item].tjsj
+            curse.teacher = res.items[item].tjrxm
+            // Ignore the waiver and make-up options
+            if (curse.grade != '放弃' && curse.grade != '补考一') {
+                totalCredits += parseFloat(res.items[item].xfjd)
+                totalFps += parseFloat(res.items[item].xf)
+            }
+            items.push(curse)
+        }
+        processedRes.items = items
+        processedRes.gpa = (totalCredits / totalFps).toFixed(2)
+        console.log(processedRes.gpa)
+        return processedRes
     }
 }
